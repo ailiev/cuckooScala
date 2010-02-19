924f4588ffee716c74382416ac8ae23ba94e51c1
Alex Iliev <alex.iliev@gmail.com>
2010-02-19 14:24:05 +0000

    Completed get and update methods. Without tracking of recursion depth in update yet.

--- src/cuckoo/CHT.scala
+++ src/cuckoo/CHT.scala
@@ -2,35 +2,90 @@ package cuckoo;
 
 import scala.collection.mutable.Map
 
-class CHT[K,V] (size : Int) extends Map[K,V] {
+class CHT[K,V] (alloc : Int) extends Map[K,V] {
 
-  var table = new Array[(K,V)](size)
+  /** How many hash functions? */
+  val H = 2
+  /** Bin size for each hash function. */
+  val B = 2
 
-  val hashes = new Array[(Int => Int)] (2)
+  val rand = new scala.util.Random
 
-  hashes(0) = x => x+1
-  hashes(1) = x => x+3
+  // the randomizing params for the hash functions.
+  val As = Array (rand.nextLong,rand.nextLong,rand.nextLong,rand.nextLong)
+  val Bs = Array (rand.nextLong,rand.nextLong,rand.nextLong,rand.nextLong)
+
+  val hashes = new Array[(Int => Int)] (H)
+
+  /** Entry will be null if no entry at that index. */
+  var table = new Array[(K,V)](alloc)
+
+  var _size = 0
+
+  for (i <- 0 until H) {
+    // Dietzfelbinger strongly universal hash function, really a small variation on
+    // a standard multiplicative hash
+    hashes(i) = x => ( ( As(i) * x.toLong + Bs(i) ) >> 32 ).toInt
+  }
+
 
   def get  (key : K) : Option[V] = {
     val hcode = key.hashCode
     for (hash <- hashes) {
-      val tableIdx = hash(hcode)
-      val tableVal = table(tableIdx)
-      if ((tableVal ne null) && tableVal._1 == key) return Some (tableVal._2)
+      val h = hash(hcode)
+      // go through the bins
+      for (i <- h until h+B) {
+        val tableVal = table(i)
+        if ((tableVal ne null) && tableVal._1 == key) return Some (tableVal._2)
+      }
     }
 
     None
   }
 
   def update (key:K, value:V) : Unit = {
-    
+    val hcode = key.hashCode
+
+    for (hash <- hashes) {
+      val h = hash(hcode)
+      // go through the bins
+      for (i <- h until h+B) {
+        val tableVal = table(i)
+        if ( (tableVal eq null) || (tableVal._1 == key) ) {
+          // found either empty slot or same key, so set new value here and we're done.
+          table(i) = (key, value)
+          _size = _size+1
+          return
+        }
+      }
+    }
+
+    // bin is full. so we:
+    // - select a bin at random
+    val binIdx = hashes(rand.nextInt(B)) (hcode)
+    // - remove the oldest entry in it, which is in first idx of the bin --> (k,v)
+    val oldEntry = table(binIdx)
+    // - slide the other entries up
+    for (i <- 0 until B-1) {
+      table(binIdx+i) = table(binIdx+i+1)
+    }
+    // - insert the new (key,value) at the end of the bin
+    table(binIdx+B-1) = (key,value)
+    // - recursively insert (k,v) into the table.
+   _size = _size+1
+    update (oldEntry._1, oldEntry._2)
   }
 
-  def size () = 7
+  def size () = _size
 
   def elements = null
 
   def -= (key : K) : Unit = {
-    
+    throw new UnsupportedOperationException("-=" + key)
   }
+}
+
+object Utils
+{
+
 }
