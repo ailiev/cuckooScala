1254dcc750092a8fef0e511524e45c62969cb80e
Alex Iliev <alex.iliev@gmail.com>
2010-02-20 00:55:46 +0000

    Small program to run the scalacheck

---
+++ test/cuckoo/RunSpec.scala
@@ -0,0 +1,9 @@
+package cuckoo
+
+object RunSpec extends Application {  
+  import org.scalacheck._  
+  import org.scalacheck.Test._  
+
+  val params = Params(100, 10, 1, 100, new java.util.Random(), 1, 1)  
+  MapSpecification.check(params)
+}
